eef294fde2fecba85b02b237385cc7a0180ff678
Pete Gautier <pete@momentohq.com>
2023-09-26 14:35:34 -0700

    cleanup and delete

--- src/DpctestBackend.php
+++ src/DpctestBackend.php
@@ -6,11 +6,12 @@ use Drupal\Core\Cache\CacheBackendInterface;
 use Drupal\Core\Logger\LoggerChannelTrait;
 use Drupal\Core\Site\Settings;
 use Drupal\Component\Serialization\SerializationInterface;
+use Drupal\Core\Cache\CacheTagsInvalidatorInterface;
 use Momento\Auth\StringMomentoTokenProvider;
 use Momento\Cache\CacheClient;
 use Momento\Config\Configurations\Laptop;
 
-class DpctestBackend implements CacheBackendInterface {
+class DpctestBackend implements CacheBackendInterface, CacheTagsInvalidatorInterface {
 
     use LoggerChannelTrait;
 
@@ -18,27 +19,24 @@ class DpctestBackend implements CacheBackendInterface {
     protected $client;
 
     public function __construct($bin) {
-        $this->getLogger('momento_cache')->notice('Constructing dpctest with bin: ' . $bin);
+        $this->getLogger('momento_cache')->info("Constructing dpctest with bin: $bin");
         $settings = Settings::get('momento_cache');
         $authToken = $settings['auth_token'];
         $authProvider = new StringMomentoTokenProvider($authToken);
         $this->bin = $bin;
         $this->client = new CacheClient(Laptop::latest(), $authProvider, 30);
-        $this->getLogger('momento_cache')->notice('Got client');
         $createResponse = $this->client->createCache($bin);
         if ($createResponse->asError()) {
             $this->getLogger('momento_cache')->error(
-                'Error creating cache ' . $bin . ': ' . $createResponse->asError()->message()
+                "Error creating cache $bin : " . $createResponse->asError()->message()
             );
         } elseif ($createResponse->asSuccess()) {
-            $this->getLogger('momento_cache')->notice('Created cache: ' . $bin);
+            $this->getLogger('momento_cache')->info("Created cache: $bin");
         }
     }
 
     public function get($cid, $allow_invalid = FALSE) {
         $this->getLogger('momento_cache')->debug('In GET with bin: ' . $this->bin);
-
-        // TODO: pass off to getMultiple
         $cids = [$cid];
         $recs = $this->getMultiple($cids);
         return reset($recs);
@@ -53,10 +51,10 @@ class DpctestBackend implements CacheBackendInterface {
             if ($getResponse->asHit()) {
                 $fetched[$cid] = unserialize($getResponse->asHit()->valueString());
                 $this->getLogger('momento_cache')->debug(
-                    "Get response (JSON encoded) for cid: " . $cid . " is: " . json_encode($fetched[$cid])
+                    "Get response (JSON encoded) for cid: $cid is: " . json_encode($fetched[$cid])
                 );
             } elseif ($getResponse->asError()) {
-                $this->getLogger('momento_cache')->debug("Get response error: " . $getResponse->asError()->message());
+                $this->getLogger('momento_cache')->error("Get response error: " . $getResponse->asError()->message());
             }
         }
         return $fetched;
@@ -75,7 +73,7 @@ class DpctestBackend implements CacheBackendInterface {
         if ($expire != CacheBackendInterface::CACHE_PERMANENT) {
             $ttl = $expire - \Drupal::time()->getRequestTime();
         }
-        $this->getLogger('momento_cache')->debug('SET TTL: ' . $ttl);
+        $this->getLogger('momento_cache')->debug("SET TTL: $ttl");
         $item->expire = $expire;
         $item->tags = $tags;
         $setResponse = $this->client->set($this->bin, $cid, serialize($item));
@@ -94,17 +92,33 @@ class DpctestBackend implements CacheBackendInterface {
     }
 
     public function delete($cid) {
-        $this->getLogger('momento_cache')->debug('In DELETE');
+        $deleteResponse = $this->client->delete($this->bin, $cid);
+        if ($deleteResponse->asError()) {
+            $this->getLogger('momento_cache')->error("DELETE response error: " . $deleteResponse->asError()->message());
+        } else {
+            $this->getLogger('momento_cache')->debug("DELETED $cid");
+        }
     }
 
     public function deleteMultiple(array $cids) {
         $this->getLogger('momento_cache')->debug('In DELETE_MULTIPLE');
-        throw new \Exception('not implemented');
+        foreach ($cids as $cid) {
+            $this->delete($cid);
+        }
     }
 
     public function deleteAll() {
         $this->getLogger('momento_cache')->debug('In DELETE_ALL');
-        throw new \Exception('not implemented');
+        // TODO: we don't have flushCache in the PHP SDK yet
+        $deleteResponse = $this->client->deleteCache($this->bin);
+        if ($deleteResponse->asError()) {
+            $this->getLogger('momento_cache')->error("DELETE_CACHE response error: " . $deleteResponse->asError()->message());
+            return;
+        }
+        $createResponse = $this->client->createCache($this->bin);
+        if ($createResponse->asError()) {
+            $this->getLogger('momento_cache')->error("CREATE_CACHE response error: " . $createResponse->asError()->message());
+        }
     }
 
     public function invalidate($cid) {
