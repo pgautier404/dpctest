d7cdf753c1206e2ca9ed871c8ef0b83d21007773
Pete Gautier <pete@momentohq.com>
2023-09-26 15:40:20 -0700

    tags

--- src/DpctestBackend.php
+++ src/DpctestBackend.php
@@ -10,6 +10,7 @@ use Drupal\Core\Cache\CacheTagsInvalidatorInterface;
 use Momento\Auth\StringMomentoTokenProvider;
 use Momento\Cache\CacheClient;
 use Momento\Config\Configurations\Laptop;
+use Momento\Requests\CollectionTtl;
 
 class DpctestBackend implements CacheBackendInterface, CacheTagsInvalidatorInterface {
 
@@ -17,9 +18,11 @@ class DpctestBackend implements CacheBackendInterface, CacheTagsInvalidatorInter
 
     protected $bin;
     protected $client;
+    private $MAX_TTL;
 
     public function __construct($bin) {
         $this->getLogger('momento_cache')->info("Constructing dpctest with bin: $bin");
+        $this->MAX_TTL = intdiv(PHP_INT_MAX, 1000);
         $settings = Settings::get('momento_cache');
         $authToken = $settings['auth_token'];
         $authProvider = new StringMomentoTokenProvider($authToken);
@@ -69,15 +72,21 @@ class DpctestBackend implements CacheBackendInterface, CacheTagsInvalidatorInter
         $item->data = $data;
         $item->created = round(microtime(TRUE), 3);
 
-        $ttl = intdiv(PHP_INT_MAX, 1000);
+        $ttl = $this->MAX_TTL;
         if ($expire != CacheBackendInterface::CACHE_PERMANENT) {
             $ttl = $expire - \Drupal::time()->getRequestTime();
         }
         $item->expire = $expire;
         $item->tags = $tags;
-        $setResponse = $this->client->set($this->bin, $cid, serialize($item));
+        $setResponse = $this->client->set($this->bin, $cid, serialize($item), $ttl);
         if ($setResponse->asError()) {
             $this->getLogger('momento_cache')->error("SET response error: " . $setResponse->asError()->message());
+        }
+        foreach ($tags as $tag) {
+            $setAddElementResponse = $this->client->setAddElement($this->bin, $tag, $cid, CollectionTtl::of($this->MAX_TTL));
+            if ($setAddElementResponse->asError()) {
+                $this->getLogger('momento_cache')->error("Error adding TAG $tag: " . $setAddElementResponse->asError()->message());
+            }
         }
     }
 
@@ -98,7 +107,7 @@ class DpctestBackend implements CacheBackendInterface, CacheTagsInvalidatorInter
         if ($deleteResponse->asError()) {
             $this->getLogger('momento_cache')->error("DELETE response error: " . $deleteResponse->asError()->message());
         } else {
-            $this->getLogger('momento_cache')->debug("DELETED $cid");
+            $this->getLogger('momento_cache')->error("DELETED $cid");
         }
     }
 
@@ -139,8 +148,24 @@ class DpctestBackend implements CacheBackendInterface, CacheTagsInvalidatorInter
     }
 
     public function invalidateTags(array $tags) {
-        $this->getLogger('momento_cache')->critical('In INVALIDATE_TAGS with tags: ' . implode(', ', $tags));
-        throw new \Exception('INVALIDATE_TAGS is not implemented: ' . implode(', ', $tags));
+        $this->getLogger('momento_cache')->error('In INVALIDATE_TAGS with tags: ' . implode(', ', $tags));
+        foreach ($tags as $tag) {
+            $this->getLogger('momento_cache')->error("Considering tag '$tag' in bin " . $this->bin);
+            $setFetchResponse = $this->client->setFetch($this->bin, $tag);
+            if ($setFetchResponse->asError()) {
+                $this->getLogger('momento_cache')->error(
+                    "Error fetching TAG $tag from bin " . $this->bin . ": " . $setFetchResponse->asError()->message()
+                );
+            } elseif ($setFetchResponse->asHit()) {
+                $cids = $setFetchResponse->asHit()->valuesArray();
+                $this->getLogger('momento_cache')->error(
+                    "Deleting $tag from bin " . $this->bin . ": " . implode(', ', $cids)
+                );
+                $this->deleteMultiple($cids);
+            } elseif ($setFetchResponse->asMiss()) {
+                $this->getLogger('momento_cache')->error("No cids found for tag $tag in bin " . $this->bin);
+            }
+        }
     }
 
     public function removeBin() {
